a950663a1e17fa73ea6e35d55ecd53219c9475ae
elhaqy <74747294+elhaqy@users.noreply.github.com>
2020-11-20 08:37:30 +0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,24 @@
+exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
+	return `⚜ ${BotName} ⚜
+  
+ʜᴀʟʟᴏ *${id.split("@s.whatsapp.net")[0]}*
+"
+
+.
+
+INFO BOT! :
+🔱 *ᴀᴜᴛʜᴏʀ*: Rafli
+🔱 *ɢɪᴛʜᴜʙ*: https://github.com/Rafli/bot-rafli
+🔱 *ɪɴsᴛᴀɢʀᴀᴍ*: ${https://Instagram/elhaqy_er?}
+🔱 *ᴛᴇʟᴇɢʀᴀᴍ* : ${http://tele.me/Rafly_er?}
+🔱 *ᴡʜᴀᴛsᴀᴘᴘ* : ${http://wa.me/6287705041961}
+
+ᴄʀᴇᴀᴛᴏʀ ʙy Rafli
+
+
+ʙᴏᴛ ᴀᴋᴛɪғ sᴇʟᴀᴍᴀ : ${kapanbotaktif}
+ᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ғᴏʟʟᴏᴡ ɪɴsᴛᴀɢʀᴀᴍ
+ ${http://Instagramn/elhaqy_er?}
+
+ ☞ᴛᴇʟᴇɢʀᴀᴍ : ${http://tele.me/Rafly_er}`
+}
